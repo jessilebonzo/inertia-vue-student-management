1c77ffc5c42d2ce8ab017b22cf88d47ca7d3cb43
Jessile Joyce Bonzo <jessilejoycebonzo25@gmail.com>
2025-01-19 12:11:35 +0800

    feat: pagination links

--- resources/js/Pages/Students/Index.vue
+++ resources/js/Pages/Students/Index.vue
@@ -1,5 +1,7 @@
 <script setup>
 import MagnifyingGlass from '@/Components/Icons/MagnifyingGlass.vue';
+import Pagination from '@/Components/Pagination.vue';
+import { usePage } from '@inertiajs/vue3';
 
 defineProps({
       students: {
@@ -7,6 +9,8 @@ defineProps({
           required: true,
       },
 });
+
+// console.log(usePage().props.students);
 </script>
 
 <template>
@@ -160,7 +164,7 @@ defineProps({
                                     </tbody>
                                 </table>
                             </div>
-                            <div>Pagination Links</div>
+                            <Pagination :data="students"/>
                         </div>
                     </div>
                 </div>
